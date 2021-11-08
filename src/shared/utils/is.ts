f998f42ead6525bcf3ada4d42644b7602e2c8507
Decemeber <39146619+Decemeber@users.noreply.github.com>
2021-11-08 12:35:39 +0800

    feat file structure org

---
+++ src/shared/utils/is.ts
@@ -0,0 +1,3 @@
+export const isFunction = (val: any) => typeof val === 'function'
+export const isObject = (val: any) => typeof val === 'object'
+export const isArray = (val: any) => Array.isArray(val)
